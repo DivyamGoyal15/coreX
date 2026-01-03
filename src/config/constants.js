bfb9de2aa3b649744a28204983216436646103ba
DivyamGoyal15 <goyaldivyam05@gmail.com>
2026-01-03 12:19:55 +0530

    initial bot upload
    initial bot upload

---
+++ src/config/constants.js
@@ -0,0 +1,38 @@
+module.exports = {
+    BRAND: {
+        NAME: 'Humnava',
+        DOMAIN: 'Create [Ticket](https://discord.com/channels/1438102520995123202/1438159018211151872) To claim Your Prize',
+        INVITE: 'dsc.gg/Humnava',
+        FOOTER: 'Enter now and don’t miss out'
+    },
+    EMOJIS: {
+        GIVEAWAY: '<:confetti:1456878119939080265>',
+        SUCCESS: '<:success:1456879523499868170>',
+        ERROR: '<:error:1456879517833494621>',
+        TIMER: '<:timer:1456881660061814845>',
+        WINNER: '<:confetti:1456878119939080265>',
+        ENTRY: '<:ENTRY:1454856743787040840>',
+        RELOAD: '<:reloading:1456882762840801290>',
+        GIFT: '<:gift:1456879520463327344>',
+        BOOST: '<:boost:1456881925640818698>'
+    },
+    COLORS: {
+        PRIMARY: '#FACC15',
+        SUCCESS: '#F6C177',
+        ERROR: '#ED4245',
+        NEUTRAL: '#FACC15'
+    },
+    LINKS: {
+        SUPPORT: 'https://dsc.gg/Humnava',
+        WEBSITE: ''
+    },
+    EMBED_STYLES: {
+        THUMBNAIL: "https://cdn.discordapp.com/attachments/1438115288355831895/1455428778741203117/giveaways.jpg",
+        IMAGE: "https://cdn.discordapp.com/attachments/1438115288355831895/1455428778741203117/giveaways.jpg"
+    },
+    BUTTON_IDS: {
+        ENTER_GIVEAWAY: 'giveaway_enter',
+        END_GIVEAWAY: 'giveaway_end',
+        REROLL_GIVEAWAY: 'giveaway_reroll'
+    }
+};
